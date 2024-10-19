068179f1c678a6f9296c1f4b915903ecaa148c92
Sam Forrest <samueljamesforrestweb@gmail.com>
2024-10-19 08:43:24 +0100

    two column flexible block (wip)

---
+++ DemoSite/Helpers/FlexibleBlockHelpers.cs
@@ -0,0 +1,16 @@
+﻿namespace DemoSite.Helpers
+{
+    public static class FlexibleBlockHelpers
+    {
+        public static string GetBlockMarginClass(DemoSite.Enums.FlexibleBlockMargin blockMargin)
+        {
+            return blockMargin switch
+            {
+                DemoSite.Enums.FlexibleBlockMargin.Small => "__smallmargin",
+                DemoSite.Enums.FlexibleBlockMargin.Medium => "__mediummargin",
+                DemoSite.Enums.FlexibleBlockMargin.Large => "__largemargin",
+                _ => ""
+            };
+        }
+    }
+}
