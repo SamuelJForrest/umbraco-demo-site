f989b9cc2a7b7975f638db620d8dc6cb363431ab
Sam Forrest <samueljamesforrestweb@gmail.com>
2024-11-22 19:42:14 +0000

    user guides package dashboard set up

---
+++ DemoSite/App_Plugins/UserGuides/user-guides.controller.js
@@ -0,0 +1,5 @@
+﻿angular.module("umbraco")
+	.controller("UserGuidesDashboardController", function ($scope) {
+		vm = this;
+		vm.title = "User Guides Test";
+	});
---
+++ DemoSite/Infrastructure/UserGuidesDashboard.cs
@@ -0,0 +1,21 @@
+﻿using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.Dashboards;
+
+namespace DemoSite.Infrastructure
+{
+	[Weight(50)]
+	public class UserGuidesDashboard : IDashboard
+	{
+		public string Alias => "userGuidesDashboard";
+		public string[] Sections => new[]
+		{
+			Constants.Applications.Content
+		};
+		public string View => "/App_Plugins/UserGuides/user-guides-dashboard.html";
+		public IAccessRule[] AccessRules => new IAccessRule[]
+		{
+			new AccessRule {Type = AccessRuleType.Grant, Value = Constants.Security.AdminGroupAlias}
+		};
+	}
+}
